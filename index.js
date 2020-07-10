49ec6fe4822d506a6249875673f7e0bc0b4e8f73
TreezDC <68121850+TreezDC@users.noreply.github.com>
2020-07-10 18:58:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
+const { Client, MessageEmbed } = require("discord.js");
+const client = new Client();
+const config = require("./config.json");
 
-const Discord = require('discord.js');
-const client = new Discord.Client();
+client.on("ready", () => {
+  console.log(`Ready logged in ${client.user.tag}!`);
+  client.on("message", async message => {
+    if (message.author.bot) return;
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+    if (!message.content.startsWith(config.prefix)) return;
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
-  }
-});
+    const args = message.content
+      .slice(config.prefix.length)
+      .trim()
+      .split(/ +/g);
+    const command = args.shift().toLowerCase();
 
-client.login('token');
+    if (command === "ping") {
+      const embed = new MessageEmbed()
+      .setTitle("Your Title")
+      .setColor("RANDOM") // Select Your color .setColor("#fff000")
+      .setDescription(`My ping is **${message.createdTimestamp - message.createdTimestamp}ms**`)// This Is ping bots
+      .setFooter("Your footer text")
+      .setTimestamp();
+      message.channel.send(embed);
+   }
